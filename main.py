8b1b0aaad5ac5fc13c05f2cc9d81ad83ef9a4ec3
santosha-2001 <139257295+santosha-2001@users.noreply.github.com>
2023-07-13 05:21:38 +0000

    added dynamic details

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 JOBS=[
   {
     'id' :'1',
@@ -26,5 +26,9 @@ app = Flask(__name__)
 @app.route("/")
 def hello():
   return render_template('home.html', jobs=JOBS)
+
+@app.route("/api/jobs")
+def list_jobs():
+  return jsonify(JOBS)
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
